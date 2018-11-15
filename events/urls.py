38b63e21cc9d112710b1195993bce64b089b4509
jankrejci <krejci.cze@gmail.com>
2018-11-15 20:43:09 +0100

    event details

--- events/urls.py
+++ events/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('events/', views.event_listing, name='events')
+    path('events/', views.event_listing, name='events'),
+    path('event/<str:name>', views.event_detail, name='detail'),
 ]
