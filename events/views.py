32b809db4d8243aa04978759d47f4247a2c5f2da
jankrejci <krejci.cze@gmail.com>
2018-11-15 20:28:09 +0100

    events listing

--- events/views.py
+++ events/views.py
@@ -4,4 +4,21 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("ahoj")
+    html = '''
+    <h1>Hey Client, my app is running!</h1>
+    <p>Check out our <a href="/events">offerings</a></p>
+    '''
+
+    return HttpResponse(html)
+
+
+def event_listing(request):
+    html = '''
+    <ul>
+        <li>Chill on the beach</li>
+        <li>Camping in the woods</li>
+        <li>Flying into the space</li>
+    </ul>
+    '''
+
+    return HttpResponse(html)
