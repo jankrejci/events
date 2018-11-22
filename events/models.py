875cc985c17b2a4154623c83d2d8a9d7c197bd75
jankrejci <krejci.cze@gmail.com>
2018-11-22 07:09:29 +0100

    vytvoreni modelu Event

--- events/models.py
+++ events/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+    name = models.CharField(max_length = 200)
+    description = models.TextField(max_length = 200)
+    location = models.CharField(max_length = 500)
+    category = models.CharField(max_length = 20)
