a867883bd5340457780aa0234c1606b3b6765a2a
jankrejci <krejci.cze@gmail.com>
2018-11-26 20:05:59 +0100

    __str__ method in events/models.py

--- events/models.py
+++ events/models.py
@@ -5,3 +5,6 @@ class Event(models.Model):
     description = models.TextField(max_length = 200)
     location = models.CharField(max_length = 500)
     category = models.CharField(max_length = 20)
+
+    def __str__(self):
+        return(self.name)
